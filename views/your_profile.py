82d5fda27cf0c230b566864e73a5ddb2c35abd7c
Sina Bösiger <boesisin@students.zhaw.ch>
2026-05-12 18:26:59 +0200

    Anpassungen

--- views/your_profile.py
+++ views/your_profile.py
@@ -1,10 +1,67 @@
 import streamlit as st
 from utils.profile_utils import load_profile, save_profile
 
+# Styling
+st.markdown("""
+<style>
+html, body, [class*="css"] {
+    font-family: 'Georgia', 'Times New Roman', serif;
+    color: #5C4033;
+}
+
+h1, h2, h3, h4, h5, h6, p, label {
+    color: #5C4033 !important;
+}
+
+.main-title {
+    text-align: center;
+    font-size: 3.4rem;
+    font-family: 'Georgia', 'Times New Roman', serif;
+    font-weight: 600;
+    color: #5C4033;
+    margin-bottom: 2rem;
+    letter-spacing: 1px;
+}
+
+/* Input Text */
+.stTextInput label {
+    color: #5C4033 !important;
+    font-family: 'Georgia', 'Times New Roman', serif;
+}
+
+/* Selectbox Label */
+.stSelectbox label {
+    color: #5C4033 !important;
+    font-family: 'Georgia', 'Times New Roman', serif;
+}
+
+/* Button */
+div.stButton > button {
+    background-color: #CDECCF;
+    color: #5C4033;
+    border-radius: 14px;
+    height: 45px;
+    font-size: 15px;
+    border: none;
+    font-family: 'Georgia', 'Times New Roman', serif;
+    font-weight: 600;
+}
+
+div.stButton > button:hover {
+    background-color: #BFE3C1;
+    color: #5C4033;
+}
+</style>
+""", unsafe_allow_html=True)
+
 username = st.session_state.get("username", "default_user")
 profile = load_profile(username)
 
-st.title("Your Profile")
+# Titel wie im anderen Code
+st.markdown(
+    "<div class='main-title'>Your Profile</div>",
+    unsafe_allow_html=True
+)
 
 name = st.text_input(
     "Name",
